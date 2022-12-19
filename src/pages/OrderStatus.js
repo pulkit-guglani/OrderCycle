ec2002e9fed90a293125e97be530857026f0a0d2
Pulkit <pulkitname@gmail.com>
2022-12-19 19:53:06 +0530

    order status page updated

--- src/pages/OrderStatus.js
+++ src/pages/OrderStatus.js
@@ -8,16 +8,35 @@ const OrderStatus = ({ status = "Pending" }) => {
   const data = location.state.finalData;
   console.log(data[0].orderItems);
   return (
-    <Box>
-      <Typography>Order No: {orderID}</Typography>
-      <Typography>
-        Items:{" "}
-        {data[0].orderItems.map((item) => (
-          <li>{item.name}</li>
-        ))}
+    <>
+      <Typography textAlign="center" variant="h5" mb="20px">
+        Order No: {orderID}
       </Typography>
-      <Typography>Status: {status}</Typography>
-    </Box>
+      <Box
+        bgcolor="primary.background.paper"
+        sx={{
+          display: "flex",
+          border: "solid",
+          marginLeft: "auto",
+          marginRight: "auto",
+          width: "fit-content",
+          padding: "10px",
+          gap: " 5vh",
+        }}
+      >
+        <Typography>
+          Items:{" "}
+          {data[0].orderItems.map((item) => (
+            <li>{item.name}</li>
+          ))}
+        </Typography>
+        <Typography>
+          <center>
+            Status: <br /> <b>{status}</b>
+          </center>
+        </Typography>
+      </Box>
+    </>
   );
 };
 
